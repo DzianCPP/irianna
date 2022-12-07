b9324fa70b20f98b7fdc920c3feae1103389b029
DzianCPP <ocherkiprozaika@gmail.com>
2022-12-07 19:15:59 +0300

    added  base-paths to bootstrap

---
+++ bootstrap/base-paths.php
@@ -0,0 +1,6 @@
+<?php
+
+const BASE_PATH = __DIR__ . "/../";
+const VIEW_PATH = __DIR__ . "/../core/view/";
+const TEMPLATES_PATH = VIEW_PATH . "templates/";
+const CACHE_PATH = VIEW_PATH . "cache/";
