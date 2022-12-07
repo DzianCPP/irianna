bf7d0c2fe00b1d1dab43c0a29f97ebe8674525fb
DzianCPP <ocherkiprozaika@gmail.com>
2022-12-07 19:44:49 +0300

    class to work with .env files

--- core/application/DotEnver.php
+++ core/application/DotEnver.php
@@ -1,5 +1,22 @@
 <?php
 
+namespace core\application;
+
+use Dotenv\Dotenv;
+
 class DotEnver
 {
+    public static function getDotEnv(): bool
+    {
+        if (!file_exists(BASE_PATH . ".env")) {
+            echo "Internal server error";
+            http_response_code(500);
+            return false;
+        }
+
+        $dotenv = Dotenv::createImmutable(BASE_PATH);
+        $dotenv->safeLoad();
+
+        return true;
+    }
 }
