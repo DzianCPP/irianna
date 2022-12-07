db2fc1e068bd40d5d501b9624522070e65b5f6be
DzianCPP <ocherkiprozaika@gmail.com>
2022-12-07 20:02:23 +0300

    minor changes

--- bootstrap/routes.php
+++ bootstrap/routes.php
@@ -1,11 +1,24 @@
 <?php
 
 use core\controllers\AppController;
+use core\controllers\buses\BusesController;
 
 return [
     '/' => [
         'controller' => AppController::class,
         'action' => 'index',
+        'method' => 'GET'
+    ],
+
+    '/buses/create' => [
+        'controller' => BusesController::class,
+        'action' => 'create',
+        'method' => 'POST'
+    ],
+
+    '/buses' => [
+        'controller' => BusesController::class,
+        'action' => 'read',
         'method' => 'GET'
     ]
 ];
