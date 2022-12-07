42b66e8bb49cb9c61f105bd98c5f08c876478a52
DzianCPP <ocherkiprozaika@gmail.com>
2022-12-07 20:28:23 +0300

    reoved excess routes

--- bootstrap/routes.php
+++ bootstrap/routes.php
@@ -8,29 +8,5 @@ return [
         'controller' => AppController::class,
         'action' => 'index',
         'method' => 'GET'
-    ],
-
-    'buses/create' => [
-        'controller' => BusesController::class,
-        'action' => 'create',
-        'method' => 'POST'
-    ],
-
-    'buses' => [
-        'controller' => BusesController::class,
-        'action' => 'read',
-        'method' => 'GET'
-    ],
-
-    'buses/delete' => [
-        'controller' => BusesController::class,
-        'action' => 'delete',
-        'method' => 'DELETE'
-    ],
-
-    'buses/update' => [
-        'controller' => BusesController::class,
-        'action' => 'update',
-        'method' => 'PUT'
     ]
 ];
