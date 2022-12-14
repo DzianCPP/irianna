4219b84d4f584a698703d53495befff123c0a80c
DzianCPP <ocherkiprozaika@gmail.com>
2022-12-14 20:34:16 +0300

    main page edited

--- core/models/countries/CountriesValidator.php
+++ core/models/countries/CountriesValidator.php
@@ -10,11 +10,11 @@ class CountriesValidator extends Validator
 
     public function isDataSafe(string $text = "", string $email = "", int|float $number = 0): bool
     {
-        $is_active = $number;
+        // $is_active = $number;
 
-        if (!is_numeric($is_active) || $is_active > 1 || $is_active < 0) {
-            return false;
-        }
+        // if (!is_numeric($is_active) || $is_active > 1 || $is_active < 0) {
+        //     return false;
+        // }
 
         return true;
     }
