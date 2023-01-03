33fd8e4d40361ad3788c2f1f2e87b89a861df8d4
DzianCPP <ocherkiprozaika@gmail.com>
2023-01-03 21:25:03 +0300

    JS Script to update managers

---
+++ public/assets/scripts/managers/update.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("save-btn").addEventListener("click", update);
+});
+
+async function update() {
+    let _name = document.getElementById("name").value;
+    let _id = document.getElementById("id").value;
+    let url = "/managers/update";
+
+    let info = {
+        name: _name,
+        id: _id
+    };
+
+    let PUT = {
+        method: "PUT",
+        body: JSON.stringify(info)
+    };
+
+    let response = await fetch(url, PUT);
+
+    if (response.ok != false) {
+        window.location = "/managers";
+    } else {
+        document.getElementById("error-field").innerHTML = "Неверно заполненные данные";
+        return;
+    }
+}
