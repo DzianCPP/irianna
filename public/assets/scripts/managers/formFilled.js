4e63d0a75a0df16c711edc889b960d16ffda9f7a
DzianCPP <ocherkiprozaika@gmail.com>
2023-01-03 21:18:49 +0300

    Refactored mFilled script

--- public/assets/scripts/managers/formFilled.js
+++ public/assets/scripts/managers/formFilled.js
@@ -1,16 +1,12 @@
-document.getElementById("last-name").addEventListener("keyup", formFilled);
-document.getElementById("second-name").addEventListener("keyup", formFilled);
-document.getElementById("first-name").addEventListener("keyup", formFilled);
+document.getElementById("name").addEventListener("keyup", formFilled);
 
 function formFilled() {
-    let lName = document.getElementById("last-name").value.length;
-    let fName = document.getElementById("first-name").value.length;
-    let sName = document.getElementById("second-name").value.length;
+    let _name = document.getElementById("name").value.length;
     let saveBtn = document.getElementById("save-btn");
 
     saveBtn.disabled = true;
 
-    if (lName + fName + sName >= 6) {
+    if (_name >= 6) {
         saveBtn.disabled = false;
     }
 }
