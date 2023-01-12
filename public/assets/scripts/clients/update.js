6e69a497bd5f99dbacdfd6d44eacca962a223214
DzianCPP <ocherkiprozaika@gmail.com>
2023-01-12 20:43:26 +0300

    js script clients->update ready

---
+++ public/assets/scripts/clients/update.js
@@ -0,0 +1,92 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("save-btn").addEventListener("click", function () {
+        update();
+    });
+});
+
+async function update() {
+    let url = "/clients/update";
+    let clients = getClients();
+
+    let PUT = {
+        method: "PUT",
+        body: JSON.stringify(clients)
+    };
+
+    let response = await fetch(url, PUT);
+
+    if (response.ok != false) {
+        window.location = "/clients";
+    } else {
+        alert("Что-то пошло не так");
+    }
+}
+
+function getClients() {
+    let clients = {
+        main_client: getMainClient(),
+        sub_client: getSubClients()
+    };
+
+    return clients;
+}
+
+function getMainClient() {
+    let _lastName = document.getElementById("main-client-last-name").value;
+    let _firstName = document.getElementById("main-client-first-name").value;
+    let _secondName = document.getElementById("main-client-second-name").value;
+    let _mainPhone = document.getElementById("main-client-phone-main").value;
+    let _secondPhone = document.getElementById("main-client-phone-second").value;
+    let _passport = document.getElementById("main-client-passport").value;
+    let _birthDate = document.getElementById("main-client-birth-date").value;
+    let _address = document.getElementById("main-client-address").value;
+
+    return {
+        name: _lastName + " " + _firstName + " " + _secondName,
+        main_phone: _mainPhone,
+        second_phone: _secondPhone,
+        passport: _passport,
+        birthday: _birthDate,
+        address: _address
+    };
+}
+
+function getSubClients() {
+    let lastNames = [];
+    let firstNames = [];
+    let secondNames = [];
+    let passports = [];
+    let birthDates = [];
+
+    for (var el of document.getElementsByName("sub-client-last-name")) {
+        lastNames.push(el.value);
+    }
+
+    for (var el of document.getElementsByName("sub-client-first-name")) {
+        firstNames.push(el.value);
+    }
+
+    for (var el of document.getElementsByName("sub-client-second-name")) {
+        secondNames.push(el.value);
+    }
+
+    for (var el of document.getElementsByName("sub-client-passport")) {
+        passports.push(el.value);
+    }
+
+    for (var el of document.getElementsByName("sub-client-birth-date")) {
+        birthDates.push(el.value);
+    }
+
+    let names = [];
+
+    for (var i = 0; i < lastNames.length; i++) {
+        names.push(lastNames[i] + " " + firstNames[i] + " " + secondNames[i]);
+    }
+    
+    return {
+        _names: names,
+        _passport: passports,
+        _birthDates: birthDates
+    };
+}
