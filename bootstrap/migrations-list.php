0731d7b3248346dcb911df8ffee4bdb0b6a215d8
DzianCPP <ocherkiprozaika@gmail.com>
2023-02-02 17:54:11 +0300

    migrations for clients and tours archives

--- bootstrap/migrations-list.php
+++ bootstrap/migrations-list.php
@@ -14,5 +14,7 @@ return [
     ['m10' => 'm10_create_templates_table'],
     ['m11' => 'm11_create_rooms_table'],
     ['m12' => 'm12_create_subclients_table'],
-    ['m13' => 'm13_create_contracts_table']
+    ['m13' => 'm13_create_contracts_table'],
+    ['m14' => 'm14_create_clients_archive_table'],
+    ['m15' => 'm15_create_tours_archive_table']
 ];
---
+++ database/migrations/m15_create_tours_archive_table.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace database\migrations;
+
+use core\application\Database;
+
+class m15_create_tours_archive_table extends BaseMigration
+{
+    public function up(): bool
+    {
+        $db = Database::getInstance();
+        $conn = $db->getConnection();
+
+        $sqlQuery = "CREATE TABLE IF NOT EXISTS tours_archive_table (
+                        id int(11) NOT NULL AUTO_INCREMENT,
+                        created text NOT NULL,
+                        manager_id int(11) DEFAULT NULL,
+                        is_only_transit int(11) DEFAULT 0,
+                        transit varchar(100) DEFAULT NULL,
+                        resort_id int(11) DEFAULT NULL,
+                        hotel_id int(11) DEFAULT NULL,
+                        checkin_date text(256) DEFAULT NULL,
+                        checkout_date text(256) DEFAULT NULL,
+                        count_of_day int(11) DEFAULT NULL,
+                        bus_id int(11) DEFAULT NULL,
+                        owner_id int(100) DEFAULT NULL,
+                        owner_travel_service int(11) DEFAULT NULL,
+                        owner_travel_cost int(11) DEFAULT NULL,
+                        number_of_children int(11) DEFAULT NULL,
+                        ages varchar(255) DEFAULT NULL,
+                        total_travel_service_byn int(11) DEFAULT NULL,
+                        total_travel_cost_byn int(11) DEFAULT NULL,
+                        total_travel_service_currency text DEFAULT NULL,
+                        total_travel_cost_currency text DEFAULT NULL,
+                        from_minsk_date text(256) DEFAULT NULL,
+                        to_minsk_date text(256) DEFAULT NULL,
+                        room_id int(11) DEFAULT NULL,                        
+                        PRIMARY KEY (id))";
+
+        $query = $conn->prepare($sqlQuery);
+
+        if (!$this->trySqlQuery($query, $conn, self::class)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function down(): bool
+    {
+        $db = Database::getInstance();
+        $conn = $db->getConnection();
+
+        $sqlQuery = "DROP TABLE IF EXISTS tours_archive_table";
+
+        $query = $conn->prepare($sqlQuery);
+
+        try {
+            $query->execute();
+            $this->migrationHistoryHandler->removeMigrationFromHistory($conn, self::class);
+        } catch (\PDOException $e) {
+            return false;
+        }
+
+        return true;
+    }
+}
