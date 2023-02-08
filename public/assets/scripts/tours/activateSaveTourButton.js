44765ce2458744208a17d6b59ed484c3c25c904e
DzianCPP <ocherkiprozaika@gmail.com>
2023-02-08 16:49:34 +0300

    done

--- public/assets/scripts/tours/activateSaveTourButton.js
+++ public/assets/scripts/tours/activateSaveTourButton.js
@@ -1,33 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("manager").addEventListener("click", function () {
+    this.body.addEventListener("mouseover", function () {
         activateSaveTourButton();
     });
-
-    document.getElementById("manager").addEventListener("click", function () {
-        activateSaveTourButton();
-    });
-
-    document.getElementById("country").addEventListener("click", function () {
-        activateSaveTourButton();
-    });
-
-    document.getElementById("resort").addEventListener("click", function () {
-        activateSaveTourButton();
-    });
-
-    document.getElementById("hotels").addEventListener("click", function () {
-        activateSaveTourButton();
-    });
-
-    document.getElementById("rooms").addEventListener("click", function () {
-        activateSaveTourButton();
-    });
-    
-    document.getElementById("departure-from-minsk").addEventListener("click", function () {
-        activateSaveTourButton();
-    });
-
-    
 });
 
 function activateSaveTourButton() {
@@ -47,6 +21,9 @@ function fieldsAreValid() {
     let hotel = document.getElementById("hotels").value;
     let room = document.getElementById("rooms").value;
     let departure_from_minsk = document.getElementById("departure-from-minsk").value;
+    let main_client_last_name = document.getElementById("main-client-last-name").value;
+    let main_client_first_name = document.getElementById("main-client-first-name").value;
+    let main_client_main_phone = document.getElementById("main-client-phone-main").value;
 
     if (!manager) {
         return false;
@@ -69,6 +46,30 @@ function fieldsAreValid() {
     }
 
     if (!departure_from_minsk) {
+        return false;
+    }
+
+    if (main_client_last_name.length < 2) {
+        return false;
+    }
+
+    if (main_client_first_name.length < 2) {
+        return false;
+    }
+
+    if (main_client_main_phone.length < 7) {
+        return false;
+    }
+
+    if (document.getElementById("main-client-passport").value.length < 4) {
+        return false;
+    }
+
+    if (!document.getElementById("main-client-birth-date").value) {
+        return false;
+    }
+
+    if (document.getElementById("total-service-cost").innerHTML.length < 1) {
         return false;
     }
     
