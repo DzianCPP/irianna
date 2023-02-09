c289c7d8bb64a9e72f6db7de76f27573160f0212
DzianCPP <ocherkiprozaika@gmail.com>
2023-02-09 18:48:59 +0300

    buttons active

--- public/assets/scripts/tours/activatePrintContractButton.js
+++ public/assets/scripts/tours/activatePrintContractButton.js
@@ -1,12 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("save-tour").addEventListener("click", function () {
-        let print_contract_btn = document.getElementById("print-contract");
-        print_contract_btn.disabled = false;
+    document.body.addEventListener("mouseover", function () {
+        if (document.getElementById("save-tour").disabled == false) {
+            let print_contract_btn = document.getElementById("print-contract");
+            print_contract_btn.disabled = false;
 
-        let print_attachement_2 = document.getElementById("print-attachment-2");
-        print_attachement_2.disabled = false;
+            let print_attachement_2 = document.getElementById("print-attachment-2");
+            print_attachement_2.disabled = false;
 
-        let print_voucher = document.getElementById("print-voucher");
-        print_voucher.disabled = false;
+            let print_voucher = document.getElementById("print-voucher");
+            print_voucher.disabled = false;
+        }
     });
 });
