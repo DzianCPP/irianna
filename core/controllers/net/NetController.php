ef11d0ac00863d2d232754324c463d2b56b2cfb9
DzianCPP <ocherkiprozaika@gmail.com>
2023-02-10 14:22:31 +0300

    Minor check

--- core/controllers/net/NetController.php
+++ core/controllers/net/NetController.php
@@ -69,8 +69,10 @@ class NetController extends BaseController
             $i = $i + 2;
         }
 
-        for ( $r = 0, $tr = 0, $d = 0; $r < count($rooms); ) {
-            $table_rows[$tr]['cols'][] = $rooms[$r]['checkin_checkout_dates'][$d];
+        for ($r = 0, $tr = 0, $d = 0; $r < count($rooms); ) {
+            if (isset($rooms[$r]) && isset($rooms[$r]['checkin_checkout_dates'][$d])) {
+                $table_rows[$tr]['cols'][] = $rooms[$r]['checkin_checkout_dates'][$d];
+            }
             $r++;
             if ($r == count($rooms)) {
                 $r = 0;
