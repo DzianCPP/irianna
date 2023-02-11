93c41c15a8f85710d03286b81251442d463338ce
DzianCPP <108674244+DzianCPP@users.noreply.github.com>
2023-02-11 16:40:35 +0300

    Rooms fix (#63)
    * net switching fixed
    
    * fixed events
    
    * if only I knew why I wrote it
    
    * dates fixed
    
    * minor fix
    
    * fixed

--- public/assets/scripts/net/net.js
+++ public/assets/scripts/net/net.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("hotels").addEventListener("click", function () {
+    document.getElementById("hotels-btn").addEventListener("click", function () {
         let url = "/net/" + document.getElementById("hotels").value;
         window.location = url;
     })
