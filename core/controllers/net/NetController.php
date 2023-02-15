32fe0a6b86cb62cfa2ebef0024c700804ff63d89
DzianCPP <ocherkiprozaika@gmail.com>
2023-02-15 18:05:49 +0300

    can' figure it out for the moment

--- core/controllers/net/NetController.php
+++ core/controllers/net/NetController.php
@@ -45,15 +45,12 @@ class NetController extends BaseController
             $clients[] = $clientsModel->get(['column' => 'id', 'value' => $tour['owner_id']])[0];
             $sub_clients[] = $clientsModel->getSubClients(['column' => 'main_client_id', 'value' => $tour['owner_id']]);
         }
-        $raw_checkin_checkout_dates = [];
 
-        foreach ($rooms as &$room) {
-            if (count($raw_checkin_checkout_dates) < count($room['checkin_checkout_dates'])) {
-                $raw_checkin_checkout_dates = $room['checkin_checkout_dates'];
-            }
-        }
+        $this->removeLettersF($rooms);
 
-        $this->magic($rooms, $tours, $clients);
+        $raw_checkin_checkout_dates = $this->getRawCheckinCheckoutDates($rooms);
+
+        $this->magic($rooms, $tours, $clients, $raw_checkin_checkout_dates);
 
         $table_headers = [];
         $table_headers[] = 'Описание номеров -->';
@@ -68,8 +65,8 @@ class NetController extends BaseController
             $table_rows[]['col_0'] = ltrim($raw_checkin_checkout_dates[$i], 'f') . ' - ' . ltrim($raw_checkin_checkout_dates[$i + 1], 'f');
             $i = $i + 2;
         }
-        
-      for ($r = 0, $tr = 0, $d = 0; $r < count($rooms); ) {
+
+        for ($r = 0, $tr = 0, $d = 0; $r < count($rooms); ) {
             if (isset($rooms[$r]) && isset($rooms[$r]['checkin_checkout_dates'][$d])) {
                 $table_rows[$tr]['cols'][] = $rooms[$r]['checkin_checkout_dates'][$d];
             }
@@ -102,33 +99,77 @@ class NetController extends BaseController
         $this->view->render("net/net.html.twig", $data);
     }
 
-    private function magic(array &$rooms, array &$tours, array &$clients): void
+    private function magic(array &$rooms, array &$tours, array &$clients, array $raws): void
     {
-
         foreach ($rooms as &$room) {
-            for ($l = 1; $l < count($room['checkin_checkout_dates']); $l += 2) {
-                $date = & $room['checkin_checkout_dates'][$l];
-                foreach ($tours as $tour) {
-                    $d = 'f' . $tour['checkout_date'];
-                    if ($d == $date && $room['id'] == $tour['room_id']) {
-                        $date = str_replace('f', 'b', $date);
-                        for ($i = 1; $i < count($room['checkin_checkout_dates']); $i += 2) {
-                            if ($room['checkin_checkout_dates'][$i] == $date) {
-                                $client = [];
-                                foreach ($clients as $c) {
-                                    if ($c['id'] == $tour['owner_id']) {
-                                        $client = $c;
-                                    }
-                                }
-
-                                if ($client != []) {
-                                    $date = str_replace($room['checkin_checkout_dates'][$i], $client['name'], $room['checkin_checkout_dates'][$i]);
-                                    $room['checkin_checkout_dates'][$i - 1] = str_replace($room['checkin_checkout_dates'][$i - 1], $client['name'], $room['checkin_checkout_dates'][$i - 1]);
-                                }
-                            }
-                        }
-                    }
+            for ($i = 1; $i < count($raws); $i+=2) {
+                if (!in_array($raws[$i], $room['checkin_checkout_dates'])) {
+                    $room['checkin_checkout_dates'][] = 'no';
+                    continue;
                 }
+            }
+        }
+        
+        // foreach ($rooms as &$room) {
+        //     for ($l = 1; $l < count($room['checkin_checkout_dates']); $l += 2) {
+        //         $date = & $room['checkin_checkout_dates'][$l];
+        //         foreach ($tours as $tour) {
+        //             $d = $tour['checkout_date'];
+        //             if ($d == $date && $room['id'] == $tour['room_id']) {
+        //                 $date = 'b' . $date;
+        //                 for ($i = 1; $i < count($room['checkin_checkout_dates']); $i += 2) {
+        //                     if ($room['checkin_checkout_dates'][$i] == $date) {
+        //                         $client = [];
+        //                         foreach ($clients as $c) {
+        //                             if ($c['id'] == $tour['owner_id']) {
+        //                                 $client = $c;
+        //                             }
+        //                         }
+
+        //                         if ($client != []) {
+        //                             $date = str_replace($room['checkin_checkout_dates'][$i], $client['name'], $room['checkin_checkout_dates'][$i]);
+        //                             $room['checkin_checkout_dates'][$i - 1] = str_replace($room['checkin_checkout_dates'][$i - 1], $client['name'], $room['checkin_checkout_dates'][$i - 1]);
+        //                         }
+        //                     }
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
+    }
+
+    private function getRawCheckinCheckoutDates(array &$rooms): array
+    {
+        $checkins = $checkouts = $raws = [];
+        
+        foreach ($rooms as &$room) {
+            for ($i = 0; $i < count($room['checkin_checkout_dates']); $i+=2) {
+                $in = $room['checkin_checkout_dates'][$i];
+                $out = $room['checkin_checkout_dates'][$i+1];
+
+                if (!in_array($in, $checkins)) {
+                    $checkins[] = $in;
+                }
+
+                if (!in_array($out, $checkouts)) {
+                    $checkouts[] = $out;
+                }
+            }
+        }
+
+        for ($i = 0; $i < count($checkins); $i++) {
+            $raws[] = $checkins[$i];
+            $raws[] = $checkouts[$i];
+        }
+        
+        return $raws;
+    }
+
+    private function removeLettersF(array &$rooms): void
+    {
+        foreach ($rooms as &$room) {
+            foreach ($room['checkin_checkout_dates'] as &$date) {
+                $date = ltrim($date, 'f');
             }
         }
     }
