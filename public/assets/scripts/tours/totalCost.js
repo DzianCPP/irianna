cc192e92b2b9f80106062cc6f517615365cdaa74
DzianCPP <ocherkiprozaika@gmail.com>
2023-02-27 19:51:55 +0300

    auto calculating on mouseover

--- public/assets/scripts/tours/totalCost.js
+++ public/assets/scripts/tours/totalCost.js
@@ -13,6 +13,21 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+    let service_costs = document.getElementsByName("sub-service-cost");
+    document.getElementById("save-tour").addEventListener("mouseover", function () {
+        let service_costs_values = 0;
+
+        for (let sc of service_costs) {
+            service_costs_values += Number(sc.value);
+        }
+
+        service_costs_values += Number(document.getElementById("main-client-service-cost").value);
+
+        document.getElementById("total-service-cost").innerHTML = service_costs_values;
+    });
+});
+
 
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -31,7 +46,21 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+    let tour_costs = document.getElementsByName("sub-tour-cost");
 
+    document.getElementById("save-tour").addEventListener("mouseover", function () {
+        let tour_costs_values = 0;
+
+        for (let tc of tour_costs) {
+            tour_costs_values += Number(tc.value);
+        }
+
+        tour_costs_values += Number(document.getElementById("main-client-tour-cost").value);
+
+        document.getElementById("total-tour-cost").innerHTML = tour_costs_values;
+    });
+});
 
 document.addEventListener("DOMContentLoaded", function () {
     window.addEventListener("load", function () {
@@ -47,10 +76,31 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
+
 document.addEventListener("DOMContentLoaded", function () {
     let tour_costs_currency = document.getElementsByName("sub-tour-cost-currency");
 
     document.getElementById("calculate-total-cost").addEventListener("click", function () {
+        let tour_costs_currency_values = 0;
+
+        for (let tc of tour_costs_currency) {
+            tour_costs_currency_values += Number(tc.value);
+        }
+
+        tour_costs_currency_values += Number(document.getElementById("main-client-total-cost-currency").value);
+
+        if (tour_costs_currency_values != 0) {
+            document.getElementById("total-cost-currency").innerHTML = tour_costs_currency_values;
+        } else {
+            document.getElementById("total-cost-currency").innerHTML = "-";
+        }
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    let tour_costs_currency = document.getElementsByName("sub-tour-cost-currency");
+
+    document.getElementById("save-tour").addEventListener("mouseover", function () {
         let tour_costs_currency_values = 0;
 
         for (let tc of tour_costs_currency) {
