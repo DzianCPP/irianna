eddc1de809fb69151aca486b54bae75a713e26ea
DzianCPP <ocherkiprozaika@gmail.com>
2023-03-06 11:12:52 +0300

    f

--- core/controllers/clients/ClientsController.php
+++ core/controllers/clients/ClientsController.php
@@ -214,14 +214,13 @@ class ClientsController extends BaseController implements ControllerInterface
         $guests = [];
         foreach ($tours as $t) {
             $cell_html = "";
-            $room = $roomsModel->get(['column' => 'id', 'value' => $t['room_id']]);
+            $room = $roomsModel->get(columnValue: ['column' => 'id', 'value' => $t['room_id']]);
             $cell_html .= "<b><u>" . $room['description'] . "</u></b><br>";
+
+            $main_client = $this->model->get(columnValue: ['column' => 'id', 'value' => $t['owner_id']]);
+            $cell_html .= "<b><u>{$main_client['name']}</u></b> - ";
+            $cell_html .= DateConverter::YMDtoDMY($main_client['birth_date']) . " - " . $main_client['passport'] . "<br>";
             $guests[] = $main_client;
-
-            // $main_client = $this->model->get(columnValue: ['column' => 'id', 'value' => $t['owner_id']]);
-            // $cell_html .= "<b><u>{$main_client['name']}</u></b> - ";
-            // $cell_html .= DateConverter::YMDtoDMY($main_client['birth_date']) . " - " . $main_client['passport'] . "<br>";
-
             // $sub_clients = $this->model->getSubClients(columnValue: ['column' => 'main_client_id', 'value' => $main_client['id']])[0];
             // foreach ($sub_clients as $sc) {
             //     $cell_html .= "<b><u>{$sc['name']}</u></b> - ";
