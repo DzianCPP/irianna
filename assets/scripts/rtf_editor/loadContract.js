08e6007ca1a785153e482a0363b5f39abda5f35c
DzianCPP <dzian.cpp@gmail.com>
2024-01-08 10:35:55 +0300

    delete assets dir

--- assets/scripts/rtf_editor/loadContract.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let contract = document.getElementById("contract-html").innerHTML;
-    if (contract) {
-        document.getElementById("text-input").innerHTML = contract;
-    }
-})
