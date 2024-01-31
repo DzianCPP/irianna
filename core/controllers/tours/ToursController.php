39fa1ee29dcb616f36cf4c7c3e214b693227098c
DzianCPP <dzian.cpp@gmail.com>
2024-01-31 20:11:18 +0300

    test: debug

--- core/controllers/tours/ToursController.php
+++ core/controllers/tours/ToursController.php
@@ -505,7 +505,6 @@ class ToursController extends BaseController implements ControllerInterface
             $contractFileName = 'templates/components/' . $fileName;
 
             $fp = fopen(BASE_PATH . $contractFileName, 'w');
-            var_dump($fp);
             fwrite($fp, $contract, strlen($contract));
             fclose($fp);
 
