6f5c1255f26868006b8686d785c90d4a1dee707e
DzianCPP <dzian.cpp@gmail.com>
2024-01-31 09:47:44 +0300

    IA-0001-fix-print-contract: echo debug message

--- core/controllers/tours/ToursController.php
+++ core/controllers/tours/ToursController.php
@@ -433,7 +433,7 @@ class ToursController extends BaseController implements ControllerInterface
 
         if ($id) {
             $tour = $toursModel->get(['column' => 'tours_table.id', 'value' => $id])[0];
-            echo json_encode($tour, JSON_PRETTY_PRINT); die();
+            echo "<pre>" . json_encode($tour, JSON_PRETTY_PRINT) . "</pre>"; die();
         } else {
             $tour = $this->model->getLastTour();
         }
