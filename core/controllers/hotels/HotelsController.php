dccddf9c3f2277859ab9f9ee702dcdef7878dce7
DzianCPP <dzian.cpp@gmail.com>
2024-02-03 18:16:00 +0300

    reverse hotels

--- core/controllers/hotels/HotelsController.php
+++ core/controllers/hotels/HotelsController.php
@@ -60,7 +60,7 @@ class HotelsController extends BaseController implements ControllerInterface
     {
         $this->setModel(HotelsModel::class);
         $resortsModel = new ResortsModel();
-        $hotels = $this->model->get(['column' => 'archived', 'value' => 0]);
+        $hotels = array_reverse($this->model->get(['column' => 'archived', 'value' => 0]));
         $this->setView(HotelsView::class);
         $page = Paginator::getPage();
         $pages = (int) ceil(count($hotels) / parent::PER_PAGE);
