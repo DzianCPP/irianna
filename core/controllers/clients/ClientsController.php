4f381b30046d462a5cbf86610843a685d08e96dd
DzianCPP <dzian.cpp@gmail.com>
2024-02-07 09:44:04 +0300

    f

--- core/controllers/clients/ClientsController.php
+++ core/controllers/clients/ClientsController.php
@@ -371,6 +371,20 @@ class ClientsController extends BaseController implements ControllerInterface
                     'value' => $client_name
                 ]
             );
+
+            $client[0][1] = $client[0]['name'];
+
+            $formattedClient = [
+                0 => $client[0]['id'],
+                1 => $client[0]['name'],
+                2 => $client[0]['main_phone'] ?? null,
+                3 => $client[0]['second_phone'] ?? null,
+                5 => $client[0]['birth_date'] ?? null,
+                4 => $client[0]['passport'] ?? null,
+                6 => $client[0]['address'] ?? null
+            ];
+
+            $client[0] = $formattedClient;
         }
 
         if (!$client || empty($client)) {
